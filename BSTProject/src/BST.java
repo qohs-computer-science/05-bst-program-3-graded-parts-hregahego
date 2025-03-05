dcc5e566aaa6d6e8731d43f4ca8f8bb62ac51f7c
hregahego <95511503+hregahego@users.noreply.github.com>
2025-03-05 14:54:50 +0000

    Part 3 submission

--- BSTProject/src/BST.java
+++ BSTProject/src/BST.java
@@ -11,74 +11,73 @@ public class BST implements BSTInterface
     public BST() {
         root = null;
         size = 0;
-    }
+    } // Creates tree with root node as null and size 0
 
     public TreeNode getRoot() { 
         return root; 
-    }
+    } // returns root node
 
     public int size() {
         return size;
-    }
+    } // returns size field
 	
 
 	public boolean isEmpty() {
-        if (size > 0) return true;
-        return false;
-    }
+        return size == 0;
+    } // if size is 0, return true
 	
 
 	public void add(Comparable newVal) {
         if (root == null) root = new TreeNode(newVal);
         else addHelper(root, newVal);
         size++;
-    }
+    } // add method; add 1 to size
 
     private void addHelper(TreeNode sub, Comparable val) {
-        if (val.compareTo(sub.getValue()) <= 0) {
-            if (sub.getLeft() == null) {
+        if (val.compareTo(sub.getValue()) <= 0) { // if value is less than current subroot, go to left child case
+            if (sub.getLeft() == null) { // if there is not already a left child, add the value as a left child
                 sub.setLeft(new TreeNode(val));
                 return;
-            } else {
+            } else { // if there is already a left child, recursion with the left child as the new subroot
                 addHelper(sub.getLeft(), val);
             }
-        } else {
+        } else { // right child case
             if (sub.getRight() == null) {
                 sub.setRight(new TreeNode(val));
             } else {
                 addHelper(sub.getRight(), val);
             }
         }
-    }
+    } // add helper function
 
 	public boolean find(Comparable toFind) {
         return (boolean) findHelper(toFind, null, root).get(0);
-    }
+    } // returns whether or not toFind was in the tree
     
     public List<Object> findHelper(Comparable value, TreeNode prev, TreeNode cur) {
         try {
-            if (value.compareTo(cur.getValue()) == 0) return Arrays.asList(true, prev, cur);
-        } catch (Exception e) {
+            if (value.compareTo(cur.getValue()) == 0) return Arrays.asList(true, prev, cur); 
+            // if the target value is equal to cur, return an array of value, previous (parent), and current node
+        } catch (Exception e) { // Means that cur is null; searched the entire tree, so value was not found
             return Arrays.asList(false, null, null);
         }
 
-        if (value.compareTo(cur.getValue()) < 0) {
+        if (value.compareTo(cur.getValue()) < 0) { // if target value is less or equal to cur, go to the left child
             return findHelper(value, cur, cur.getLeft());
-        } else {
+        } else { // right child case
             return findHelper(value, cur, cur.getRight());
         }
     }
 
 	public boolean replace(Comparable old, Comparable toAdd) {
-        if (!find(old)) return false;
-        delete(old);
+        boolean found = delete(old);
         add(toAdd);
-        return true;
-    }
+        return found;
+    } // Delete old value and add the new value
 
     private int wahoo(Object x) {
         return (x == null) ? 0 : 1;
-    }
+    } // if x is null, return 0; else return 1. Used to concisely count number of children nodes
 	public boolean delete(Comparable old) {
         
         List<Object> result = findHelper(old, null, root);
@@ -96,10 +95,10 @@ public class BST implements BSTInterface
             isLeftChild = (parent.getLeft() == target) ? true : false;
         } catch (Exception e) {
             isLeftChild = false;
-        }
+        } // determine whether the node to be deleted is a left or right child
         
         switch (numChildren) {
-            case 0:
+            case 0: // target node has 0 children case
                 if (isLeftChild) {
                     parent.setLeft(null);
                 } else {
@@ -107,77 +106,78 @@ public class BST implements BSTInterface
                 }
                 break;
             
-            case 1:
+            case 1: // target node has 1 child case
                 TreeNode child = (target.getLeft() == null) ? target.getRight() : target.getLeft();
                 if (isLeftChild) {
                     parent.setLeft(child);
                 } else {
                     parent.setRight(child);
-                }
+                } // The child node of the parent node is set to the target node's child
                 break;
 
-            case 2:
+            case 2: // target node has 2 children case
                 TreeNode LC = target.getLeft();
                 TreeNode RC = target.getRight();
 
                 TreeNode rightmost = LC;
                 while (rightmost.getRight() != null) {
                     rightmost = rightmost.getRight();
-                }
+                } // Gets the rightmost node of the left subtree
 
                 rightmost.setRight(RC);
                 target.setValue(LC.getValue());
                 target.setLeft(LC.getLeft());
                 target.setRight(LC.getRight());
-        }
+        } // switch statement to handle each case
         size--;
         return true;
 
-    }
+    } // method to delete nodes
 
 	public void printInOrder() {
         if (root != null) inorder(root);
-        
-    }
+    } // prints inorder traversal
 
     private void inorder(TreeNode sub) {
 
         if (sub.getLeft() != null) {
             inorder(sub.getLeft());
-        }
+        } // Traverse left sub tree
 
         System.out.println(sub.getValue());
 
         if (sub.getRight() != null) {
             inorder(sub.getRight());
-        }
-    }
+        } // Traverse right sub tree
+    } // inorder traversal recursion
     
 	public void printPreOrder() {
         if (root != null) preorder(root);
-    }
+    } // prints preorder traversal
 
     private void preorder(TreeNode sub) {
         System.out.println(sub.getValue());
         if (sub.getLeft() != null) {
             preorder(sub.getLeft());
-        }
+        } // traverse left subtree
         if (sub.getRight() != null) {
             preorder(sub.getRight());
-        }
-    }
+        }  // traverse right subtree
+    } // preorder traversal recursion
 
 	public void printPostOrder() {
         if (root != null) postorder(root);
-    }
+    } // pprints postorder traversal
     private void postorder(TreeNode sub) {
         if (sub.getLeft() != null) {
             postorder(sub.getLeft());
-        } if (sub.getRight() != null) {
+        } //traverse left subtree
+        
+        if (sub.getRight() != null) {
             postorder(sub.getRight());
-        }
+        } // traverse right subtree
         System.out.println(sub.getValue());
-    }
+    } // postorder traversal recursion
 
 
 }
--- BSTProject/src/BSTTester.java
+++ BSTProject/src/BSTTester.java
@@ -10,28 +10,33 @@ import java.lang.Comparable;
 public class BSTTester {
     public static void main(String[] args) {
         BST test = new BST();
-        String add = "ALGBEIQPS";
+        String add = "ORCHARDQUINCE";
         for (char c : add.toCharArray()) {
             test.add(c);
-        }
-//              A          
-//               \
-//                L
-//               /  \
-//              G    Q
-//             / \   /\
-//            B   I P  S    
-//             \
-//              E
-//
-//
-//
-//
-//
-        // TreeNode result = (TreeNode) test.findHelper('G', null, test.getRoot()).get(1);
-        // System.out.println(result.getValue());
-        // test.delete('');
-        test.delete('L');
+        } // loop through chararray to add each letter
+
+        System.out.println("Size of tree: " + test.size());
+        System.out.println("Is tree empty? "  + test.isEmpty());
+
+        System.out.println("Preorder Traversal");
         test.printPreOrder();
-    }
-}
+        System.out.println("Inorder Traversal");
+        test.printInOrder();
+        System.out.println("Postorder Traversal");
+        test.printPostOrder();
+
+        System.out.println("Find Z: " + test.find('Z'));
+        System.out.println("Find Q: " + test.find('Q'));
+
+        System.out.println("================================================");
+        System.out.println("Deleting D: ");
+        test.delete('D');
+        test.printInOrder();
+        System.out.println("Replacing Q with Y: ");
+        test.replace('Q', 'Y');
+        test.printInOrder();
+        System.out.println("Final size: " + test.size());
+
+
+    } // end main
+} // end class
